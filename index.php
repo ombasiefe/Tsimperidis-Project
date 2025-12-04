295cc7293c2435bded54e831e4b076de723dab28
billie115 <you@example.com>
2025-12-04 13:04:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    echo"I love niggers <br>";
+    echo"They do good work";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>
+        order a nigger
+    </button>
+</body>
+</html>
